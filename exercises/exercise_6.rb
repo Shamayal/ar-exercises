45dbcb830bc769d4aeb79ddc097357bdd6bece34
Shamayal Syed <shamayalsyed9@gmail.com>
2023-06-01 12:23:30 -0400

    exercise 6 - added employees for store 1 and 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -12,7 +12,9 @@ puts "----------"
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "Mina", last_name: "Patel", hourly_rate: 70)
 @store1.employees.create(first_name: "Olivia", last_name: "Martinez", hourly_rate: 55)
+@store1.employees.create(first_name: "Vivien", last_name: "Nguyen", hourly_rate: 75)
 
 @store2.employees.create(first_name: "Sofia", last_name: "Almeida", hourly_rate: 80)
 @store2.employees.create(first_name: "Richard", last_name: "Kim", hourly_rate: 75)
 @store2.employees.create(first_name: "Aisha", last_name: "Khan", hourly_rate: 65)
+@store2.employees.create(first_name: "Ethan", last_name: "Thompson", hourly_rate: 50)
